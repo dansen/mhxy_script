3ec81f3c9e217b37c491af97720b44d99e4f0e46
dansen <370283593@qq.com>
2024-02-26 14:34:48 +0800

    open ui

--- ui/mhxy_pyqt.py
+++ ui/mhxy_pyqt.py
@@ -1,5 +1,12 @@
 import os
 import sys
+import inspect
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+sys.path.insert(0, parentdir+"/..")
+
 from configparser import ConfigParser
 
 from PyQt6 import QtWidgets
